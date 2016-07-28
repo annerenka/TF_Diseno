06ab046839d97d083e60b24f7791c98fcda2658c
njares <nico.jar@gmail.com>
2016-07-28 19:10:21 -0300

    Update salientes.m

--- octave/salientes.m
+++ octave/salientes.m
@@ -1,3 +1,10 @@
+## Copyright (C) 2015 Nicolás Jares
+##
+## Copying and distribution of this file, with or without modification,
+## are permitted in any medium without royalty provided the copyright
+## notice and this notice are preserved.  This file is offered as-is,
+## without any warranty.
+
 # Uso:
 #
 #	y=salientes(i,A)
