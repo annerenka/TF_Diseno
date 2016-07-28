0280612015bafa028f8bc7836b842871eb81a515
njares <nico.jar@gmail.com>
2016-07-28 19:04:19 -0300

    Update genera_matriz_A.m

--- octave/genera_matriz_A.m
+++ octave/genera_matriz_A.m
@@ -1,17 +1,24 @@
+## Copyright (C) 2015 Nicolás Jares
+##
+## Copying and distribution of this file, with or without modification,
+## are permitted in any medium without royalty provided the copyright
+## notice and this notice are preserved.  This file is offered as-is,
+## without any warranty.
+
 # Uso:
 #
 #	genera_matriz_A(input)
 #
 # Donde "input" debe ser una ruta a un archivo.
-# Toma el archivo input, una matriz de nx2 de %*índices*) de 
+# Toma el archivo input, una matriz de nx2 de índices de 
 # aristas, y genera 1 archivo:
 #	1- "matriz_A": un archivo que contiene una matriz de 
 #		2nx3, donde n es la cantidad de aristas. De los tres
 #		elementos de cada fila, los primeros dos son la 
-#		%*posición*) del elemento en la matriz_A, cuyo valor es el
+#		posición del elemento en la matriz_A, cuyo valor es el
 #		tercer elemento de la fila correspondiente.
 #			
-# sparse(matriz_A(:,1),matriz_A(:,2),matriz_A(:,3)) %*será*)
+# sparse(matriz_A(:,1),matriz_A(:,2),matriz_A(:,3)) será
 # una matriz de nxm, donde n es la cantidad de aristas y 
 # m es la cantidad de nodos. Cada fila tiene un 1 en la 
 # columna correspondiente al nodo donde se incia la 
