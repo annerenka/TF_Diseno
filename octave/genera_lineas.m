4448eeb055f5dace0798ef2d6261f0be70bd6297
njares <nico.jar@gmail.com>
2016-07-28 19:03:33 -0300

    Update genera_lineas.m

--- octave/genera_lineas.m
+++ octave/genera_lineas.m
@@ -1,13 +1,20 @@
+## Copyright (C) 2015 Nicolás Jares
+##
+## Copying and distribution of this file, with or without modification,
+## are permitted in any medium without royalty provided the copyright
+## notice and this notice are preserved.  This file is offered as-is,
+## without any warranty.
+
 # Uso:
 #
 #	genera_lineas(n)
 #
-# Donde "n" debe ser un %*número*) entero.
+# Donde "n" debe ser un número entero.
 # Toma archivos de la forma "linea_i", donde i va desde 1 
 # hasta n. Cada archivo debe contener una matriz de (m_i)x2, 
 # donde en cada fila esten los indices de dos aristas que 
 # compartan el nodo de una parada que pertenezca a la 
-# linea_i. Las paradas %*así*) descritas deben estar en el orden 
+# linea_i. Las paradas así descritas deben estar en el orden 
 # en el que son recorridas. Se genera 1 archivo:
 #	1- "lineas": un archivo que contiene una matriz de 
 #		(m+1)x2n, donde m es el mayor m_i encontrado. Cada 
